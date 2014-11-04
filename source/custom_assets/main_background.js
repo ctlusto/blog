90030e28bcf5c3c80a803b267a5247e15e496b34
Chris Lusto <ctlusto@gmail.com>
2014-11-03 22:53:18 -0500

    Add main dynamic background

--- source/custom_assets/main_background.js
+++ source/custom_assets/main_background.js
@@ -1,1 +1,122 @@
-$(function() {});
+$(function() {
+
+    var $main = $('#main');
+    $main.append("<div id='world'></div>");
+    $main.css('backgroundColor', 'transparent');
+
+    var $world = $('#world');
+    $world.width($main.outerWidth()).height($main.outerHeight());
+    $world.css('opacity', 0.75);
+
+    Physics(function (world) {
+
+    var viewWidth = $world.width()
+        ,viewHeight = $world.height()
+        // center of the window
+        ,center = Physics.vector(viewWidth, viewHeight).mult(0.5)
+        // bounds of the window
+        ,viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight)
+        ,attractor
+        ,edgeBounce
+        ,renderer
+        ;
+
+    // create a renderer
+    renderer = Physics.renderer('canvas', {
+        el: 'world'
+        ,width: viewWidth
+        ,height: viewHeight
+    });
+
+    // add the renderer
+    world.add(renderer);
+    // render on each step
+    world.on('step', function () {
+        world.render();
+    });
+
+    // attract bodies to a point
+    attractor = Physics.behavior('attractor', {
+        pos: center
+        ,strength: .1
+        ,order: 1
+    });
+
+    // constrain objects to these bounds
+    edgeBounce = Physics.behavior('edge-collision-detection', {
+        aabb: viewportBounds
+        ,restitution: 0.2
+        ,cof: 0.8
+    });
+
+    // resize events
+    window.addEventListener('resize', function () {
+
+        viewWidth = $main.outerWidth();
+        viewHeight = $main.outerHeight();;
+
+        $world.width(viewWidth).height(viewHeight);
+
+        renderer.el.width = viewWidth;
+        renderer.el.height = viewHeight;
+
+        viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+        // update the boundaries
+        edgeBounce.setAABB(viewportBounds);
+
+    }, true);
+
+    // move the attractor position to match the mouse coords
+    $main.mousemove(function(e){
+        attractor.position({ x: e.clientX, y: e.clientY });
+    });
+
+    // create some bodies
+    var l = 20;
+    var bodies = [];
+    var v = Physics.vector(0, 300);
+    var b, r;
+
+    while ( l-- ) {
+        r = (20 + Math.random()*30)|0;
+        b = Physics.body('circle', {
+            radius: r
+            ,mass: r
+            ,x: v.x + center.x
+            ,y: v.y + center.y
+            ,vx: v.perp().mult(0.0001).x
+            ,vx: v.y
+            ,styles: {
+                fillStyle: '#4f81bd'
+            }
+        });
+        bodies.push(b);
+        v.perp(true)
+            .mult(10000)
+            .rotate(l / 3);
+    }
+
+    // add things to the world
+    world.add( bodies );
+    world.add([
+        Physics.behavior('newtonian', {
+            strength: 0.005
+            ,min: 10
+        })
+        ,Physics.behavior('body-impulse-response')
+        ,edgeBounce
+        ,attractor
+    ]);
+
+    // subscribe to ticker to advance the simulation
+    Physics.util.ticker.on(function( time ) {
+        world.step( time );
+    });
+
+    // start the ticker
+    Physics.util.ticker.start();
+});
+
+
+
+});
