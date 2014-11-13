eaf35bf1bfef9ef5b6b5201ef4126dd04688c88a
Chris Lusto <ctlusto@gmail.com>
2014-11-12 23:54:58 -0500

    Add console greeting

---
+++ source/javascripts/greeting.js
@@ -0,0 +1,18 @@
+(function() {
+    var s = "";
+    s += "                  _   _ \n";                       
+    s += "  _ __ ___   __ _| |_| |__    _ __  _   _ _ __  \n";
+    s += " | '_ ` _ \\ / _` | __| '_ \\  | '_ \\| | | | '_ \\ \n";
+    s += " | | | | | | (_| | |_| | | | | |_) | |_| | | | | \n";
+    s += " |_| |_| |_|\\__,_|\\__|_| |_| | .__/ \\__,_|_| |_| \n";
+    s += "                             |_| ";
+
+    s += "\n";
+    s += "\n";
+    s += "Hello there, console explorer! If you're interested in peeping any of the code on or powering ";
+    s += "this site, I've tried to make that simple for you to do. In fact, the source for everything you see here ";
+    s += "is available at http://github.com/ctlusto/blog. Go check it out. It's easier on the eyes.";
+    s += "\n";
+
+    console.log(s);
+})();
